6bac7b9c6a5921ed94ee36ccf8b6dec48658ea35
Lucas García Rodríguez <lucasgarciarodriguez@gmail.com>
2016-02-06 19:19:44 +0100

    Changes to energyplot synthax
    Other:
    The energyplot synthax has been adapted to be used by a future App.

--- @tsphopfieldnet/energyplot.m
+++ @tsphopfieldnet/energyplot.m
@@ -1,10 +1,18 @@
-function energyplot(net)
+function energyplot(net,ax)
     if net.results.validPath
-        figure;
+        if isempty(ax)
+            figure;
+        end
         toBePlotted = ~isnan(net.results.energy) & net.results.time < 10^100;
-        semilogy(net.results.time(toBePlotted), net.results.energy(toBePlotted), '.b-');
-        title('Energy Function');
-        xlabel('Time');
+        if isempty(ax)
+            semilogy(net.results.time(toBePlotted), net.results.energy(toBePlotted), '.b-');
+            title('Energy Function');
+            xlabel('Time');
+        else
+            semilogy(ax,net.results.time(toBePlotted), net.results.energy(toBePlotted), '.b-');
+            title(ax,'Energy Function');
+            % xlabel(ax,'Time');
+        end
         axis tight;
     else
         error('tsphopfieldnet:energyplot','Unvalid energy results. Possibly no path has been found or training has not taken place yet.');
