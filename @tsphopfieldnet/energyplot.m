39daed798ede63d95987643179e587ef1d83b1ee
Lucas García Rodríguez <lucasgarciarodriguez@gmail.com>
2016-02-07 11:25:39 +0100

    Minor bug fix
    Bug fixing:
    #19: energyplot can now be properly used from the command line. It's
    synthax has been updated to be used by a future App.

--- @tsphopfieldnet/energyplot.m
+++ @tsphopfieldnet/energyplot.m
@@ -1,4 +1,9 @@
-function energyplot(net,ax)
+function energyplot(net,varargin)
+    if nargin > 1
+        ax = varargin{1};
+    else
+        ax = [];
+    end
     if net.results.validPath
         if isempty(ax)
             figure;
@@ -8,12 +13,13 @@ function energyplot(net,ax)
             semilogy(net.results.time(toBePlotted), net.results.energy(toBePlotted), '.b-');
             title('Energy Function');
             xlabel('Time');
+            axis tight;
         else
             semilogy(ax,net.results.time(toBePlotted), net.results.energy(toBePlotted), '.b-');
             title(ax,'Energy Function');
             % xlabel(ax,'Time');
-        end
-        axis tight;
+            axis(ax,'tight')
+        end       
     else
         error('tsphopfieldnet:energyplot','Unvalid energy results. Possibly no path has been found or training has not taken place yet.');
     end
